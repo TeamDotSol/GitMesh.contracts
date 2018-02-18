700cf3100e0a2eebaa4cf5a852223982380123f2
leondo <leondo123@gmail.com>
2018-02-18 01:53:01 -0700

    deploy to rinkeby

--- truffle.js
+++ truffle.js
@@ -1,9 +1,17 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "your seed phrase here";
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/AQLPHGoZNh6Ktd33vkIg"),
+      network_id: 3
     }
   }
 };
